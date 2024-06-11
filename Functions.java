e3fde5e85f7e09b5292143865feb349f676b0f5c
vaishnavi manikeri <155414266+VaishnaviManikeri@users.noreply.github.com>
2024-06-11 15:03:34 +0530

    call fun() with creating  method in another class

---
+++ Functions.java
@@ -0,0 +1,16 @@
+package function;
+
+public class Functions {
+	public  static void add() {
+		int a=10;
+		int b=30;
+		int total=a+b;
+		System.out.println("total  of add is="+total);
+	}
+	public  static void sub(int i,int j) {
+		int a=i;
+		int b=j;
+		int total1=a-b;
+		System.out.println("total  of sub is="+total1);
+}
+}
---
+++ Testfunction.java
@@ -0,0 +1,12 @@
+package function;
+
+public class Testfunction {
+
+	public static void main(String[] args) {
+System.out.println("hello");
+Functions f=new Functions();
+f.add();
+f.sub(12,5);
+	}
+
+}
